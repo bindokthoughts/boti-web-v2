be17c936da5f0066d50a8803e4472cf812714123
bindokthoughts <bindokthoughts@gmail.com>
2026-05-07 00:51:12 +0530

    Contact section repeatation removed

--- src/components/templates/BaseLayout.tsx
+++ src/components/templates/BaseLayout.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Navbar from '../organisms/Navbar';
 import Footer from '../organisms/Footer';
-import Contact from '../organisms/Contact';
+
 import MasterScene from '../organisms/MasterScene';
 import CustomCursor from '../atoms/CustomCursor';
 
@@ -15,7 +15,7 @@ const BaseLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <div id="root-layout" className="min-h-screen selection:bg-primary/20 selection:text-primary relative z-10">
         <Navbar />
         <main>{children}</main>
-        <Contact />
+
         <Footer />
       </div>
     </>
